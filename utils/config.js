c21c140d4e5fcdc5ec63dc916559318b13f7be05
yinhang <yinhang@dehuinet.com>
2018-01-11 11:17:25 +0800

    vue seed and path config

---
+++ utils/config.js
@@ -0,0 +1,9 @@
+const root = require('user-home');
+
+const TEMP_NAMESPACE = `minxing-devtools`;
+const TEAMPLATE_TEMP = `${root}/${TEMP_NAMESPACE}/templates`;
+const LOCALSTORAGE_TEMP = `${root}/${TEMP_NAMESPACE}/localstorage`;
+
+module.exports = {
+    TEAMPLATE_TEMP
+}
